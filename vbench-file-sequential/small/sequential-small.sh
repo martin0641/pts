61b6549b7e3f8ddd409e9ecb356f738f9e13fa6a
martin0641 <martin0641@gmail.com>
2021-12-28 15:37:21 -0500

    Many

--- vbench-file-sequential/small/sequential-small.sh
+++ vbench-file-sequential/small/sequential-small.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-rm -Rf /gpfs/fs1/pts/sequential-small/
-rm -Rf /gpfs/fs2/pts/sequential-small/
-mkdir /gpfs/fs1/pts/
-mkdir /gpfs/fs1/pts/sequential-small/
-mkdir /gpfs/fs2/pts/
-mkdir /gpfs/fs2/pts/sequential-small/
+rm -Rf /gpfs/data/pts/sequential-small/
+rm -Rf /gpfs/data/pts/sequential-small/
+mkdir /gpfs/data/pts/
+mkdir /gpfs/data/pts/sequential-small/
+mkdir /gpfs/data/pts/
+mkdir /gpfs/data/pts/sequential-small/
 
-time dd if=/dev/urandom of=/gpfs/fs1/pts/sequential-small/sequential-small.file bs=1M count=1024 > /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
+time dd if=/dev/urandom of=/gpfs/data/pts/sequential-small/sequential-small.file bs=1M count=1024 > /gpfs/data/pts/sequential-small/sequential-small-results.txt
 
-sha1sum /gpfs/fs1/pts/sequential-small/sequential-small.file >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt > /gpfs/fs1/pts/sequential-small/sequential-small.file.sha1
-#time cp --force --verbose /gpfs/fs1/pts/sequential-small/sequential-small.file /gpfs/fs2/pts/sequential-small/sequential-small.file >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
-rsync -a --info=progress2 --stats /gpfs/fs1/pts/sequential-small/sequential-small.file /gpfs/fs2/pts/sequential-small/sequential-small.file >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
-rsync -a --info=progress2 --stats /gpfs/fs2/pts/sequential-small/sequential-small.file /gpfs/fs1/pts/sequential-small/sequential-small.file.sanity >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
-sha1sum /gpfs/fs1/pts/sequential-small/sequential-small.file.sanity >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt > /gpfs/fs1/pts/sequential-small/sequential-small.file.sanity.sha1
-echo "CMP says:" >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
-cmp -b /gpfs/fs1/pts/sequential-small/sequential-small.file /gpfs/fs1/pts/sequential-small/sequential-small.file.sanity >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
-echo "If it's blank, no news is good news" >> /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
-cat /gpfs/fs1/pts/sequential-small/sequential-small-results.txt
+sha1sum /gpfs/data/pts/sequential-small/sequential-small.file >> /gpfs/data/pts/sequential-small/sequential-small-results.txt > /gpfs/data/pts/sequential-small/sequential-small.file.sha1
+#time cp --force --verbose /gpfs/data/pts/sequential-small/sequential-small.file /gpfs/data/pts/sequential-small/sequential-small.file >> /gpfs/data/pts/sequential-small/sequential-small-results.txt
+rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-small/sequential-small.file /gpfs/data/pts/sequential-small/sequential-small.file >> /gpfs/data/pts/sequential-small/sequential-small-results.txt
+rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-small/sequential-small.file /gpfs/data/pts/sequential-small/sequential-small.file.sanity >> /gpfs/data/pts/sequential-small/sequential-small-results.txt
+sha1sum /gpfs/data/pts/sequential-small/sequential-small.file.sanity >> /gpfs/data/pts/sequential-small/sequential-small-results.txt > /gpfs/data/pts/sequential-small/sequential-small.file.sanity.sha1
+echo "CMP says:" >> /gpfs/data/pts/sequential-small/sequential-small-results.txt
+cmp -b /gpfs/data/pts/sequential-small/sequential-small.file /gpfs/data/pts/sequential-small/sequential-small.file.sanity >> /gpfs/data/pts/sequential-small/sequential-small-results.txt
+echo "If it's blank, no news is good news" >> /gpfs/data/pts/sequential-small/sequential-small-results.txt
+cat /gpfs/data/pts/sequential-small/sequential-small-results.txt
