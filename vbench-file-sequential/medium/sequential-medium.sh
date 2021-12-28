ac9ca63b2111b37fda94291a4bd165b9a6aceecb
martin0641 <martin0641@gmail.com>
2021-12-28 16:36:14 -0500

    1

---
+++ vbench-file-sequential/medium/sequential-medium.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+rm -Rf /gpfs/cache/pts/sequential-medium/
+rm -Rf /gpfs/data/pts/sequential-medium/
+mkdir /gpfs/cache/pts/
+mkdir /gpfs/cache/pts/sequential-medium/
+mkdir /gpfs/data/pts/
+mkdir /gpfs/data/pts/sequential-medium/
+
+time dd if=/dev/urandom of=/gpfs/cache/pts/sequential-intermediate.file bs=1M count=1024 > /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
+
+for i in 1 2 3 4 5
+do
+   cat /gpfs/cache/pts/sequential-medium/sequential-intermediate.file >> /gpfs/cache/pts/sequential-medium/sequential-medium.file
+done
+
+sha1sum /gpfs/cache/pts/sequential-medium/sequential-medium.file >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt > /gpfs/cache/pts/sequential-medium/sequential-medium.file.sha1
+#time cp --force --verbose /gpfs/data/pts/sequential-medium/sequential-medium.file /gpfs/data/pts/sequential-medium/sequential-medium.file >> /gpfs/data/pts/sequential-medium/sequential-medium-results.txt
+rsync -a --info=progress2 --stats /gpfs/cache/pts/sequential-medium/sequential-medium.file /gpfs/data/pts/sequential-medium/sequential-medium.file >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
+rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-medium/sequential-medium.file /gpfs/cache/pts/sequential-medium/sequential-medium.file.sanity >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
+sha1sum /gpfs/cache/pts/sequential-medium/sequential-medium.file.sanity >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt > /gpfs/cache/pts/sequential-medium/sequential-medium.file.sanity.sha1
+echo "CMP says:" >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
+cmp -b /gpfs/cache/pts/sequential-medium/sequential-medium.file /gpfs/cache/pts/sequential-medium/sequential-medium.file.sanity >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
+echo "If it's blank, no news is good news" >> /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
+cat /gpfs/cache/pts/sequential-medium/sequential-medium-results.txt
