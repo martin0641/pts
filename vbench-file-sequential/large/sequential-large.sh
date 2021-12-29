b68a81170e93ddb35992ac84e03489d622edbc9a
martin0641 <martin0641@gmail.com>
2021-12-28 21:50:42 -0500

    1

--- vbench-file-sequential/large/sequential-large.sh
+++ vbench-file-sequential/large/sequential-large.sh
@@ -8,7 +8,7 @@ mkdir /gpfs/data/pts/sequential-large/
 
 time dd if=/dev/urandom of=/gpfs/cache/pts/sequential-large/sequential-intermediate.file bs=1M count=1024 > /gpfs/cache/pts/sequential-large/sequential-large-results.txt
 
-for i in 1 2 3 4 5 6 7 8 9
+for i in 1 2 3 4 5 6 7 8 9 10
 do
    cat /gpfs/cache/pts/sequential-large/sequential-intermediate.file >> /gpfs/cache/pts/sequential-large/sequential-large.file
 done
@@ -16,21 +16,21 @@ done
 sha1sum /gpfs/cache/pts/sequential-large/sequential-large.file >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt > /gpfs/cache/pts/sequential-large/sequential-large.file.sha1
 rsync -a --info=progress2 --stats /gpfs/cache/pts/sequential-large/sequential-large.file /gpfs/data/pts/sequential-large/sequential-large.file1 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
 rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-large/sequential-large.file1 /gpfs/cache/pts/sequential-large/sequential-large.file2 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file1
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file1
 rsync -a --info=progress2 --stats /gpfs/cache/pts/sequential-large/sequential-large.file2 /gpfs/data/pts/sequential-large/sequential-large.file3 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file2
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file2
 rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-large/sequential-large.file13 /gpfs/cache/pts/sequential-large/sequential-large.file4 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file3
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file3
 rsync -a --info=progress2 --stats /gpfs/cache/pts/sequential-large/sequential-large.file4 /gpfs/data/pts/sequential-large/sequential-large.file5 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file4
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file4
 rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-large/sequential-large.file15 /gpfs/cache/pts/sequential-large/sequential-large.file6 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file5
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file5
 rsync -a --info=progress2 --stats /gpfs/cache/pts/sequential-large/sequential-large.file6 /gpfs/data/pts/sequential-large/sequential-large.file7 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file6
-rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-large/sequential-large.file17 /gpfs/cache/pts/sequential-large/sequential-large.file8 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file7
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file6
+rsyncf -a --info=progress2 --stats /gpfs/data/pts/sequential-large/sequential-large.file17 /gpfs/cache/pts/sequential-large/sequential-large.file8 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file7
 rsync -a --info=progress2 --stats /gpfs/cache/pts/sequential-large/sequential-large.file8 /gpfs/data/pts/sequential-large/sequential-large.file9 >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
-rm -r /gpfs/data/pts/sequential-large/sequential-large.file8
+rm -f /gpfs/data/pts/sequential-large/sequential-large.file8
 rsync -a --info=progress2 --stats /gpfs/data/pts/sequential-large/sequential-large.file9 /gpfs/cache/pts/sequential-large/sequential-large.file.sanity >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
 sha1sum /gpfs/cache/pts/sequential-large/sequential-large.file.sanity >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt > /gpfs/cache/pts/sequential-large/sequential-large.file.sanity.sha1
 echo "CMP says:" >> /gpfs/cache/pts/sequential-large/sequential-large-results.txt
